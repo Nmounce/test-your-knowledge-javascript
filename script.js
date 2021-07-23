8926582d7ae519f54c365721a1ae1b211a8ff8c2
NikkiMounce <nikkimounce@gmail.com>
2021-07-22 21:29:13 -0700

    almost have the functionality down...

--- script.js
+++ script.js
@@ -30,13 +30,12 @@ function startTimer() {
             clearInterval(timer);
             goToResultBox.style.display = "block";
         }
-}, 1000);
-
+    }, 1000);
+}
 //function for start button (id="start-btn")-> navigate from rules box to quiz box (id="quiz-box")
 
 var startBtn = document.querySelector("#start-btn");
 var goToQuizBox = document.querySelector("#quiz-box");
-var countDown = timer;
 startBtn.onclick = () => {
     if (goToQuizBox.style.display !== "none") {
         goToQuizBox.style.display = "none";
@@ -45,7 +44,7 @@ startBtn.onclick = () => {
         goToRulesBox.style.display = "none";
     }
     nextQuestion();
-    window.onload = startTimer(); 
+    window.onload = startTimer();
 
 };
 
@@ -121,7 +120,6 @@ let questions = [
 
 //function to shuffle questions
 let shuffledQuestions = [];
-
 function handleQuestions() {
     while (shuffledQuestions.length <=7) {
         const random = questions[Math.floor(Math.random() * questions.length)];
@@ -134,19 +132,18 @@ function handleQuestions() {
 //function for displaying next question
 let questionNumber = 1;
 let playerScore = 0;
-let wrongAttempt = 0;
 let indexNumber = 0;
 
 function nextQuestion(index) {
     handleQuestions();
     const currentQuestion = shuffledQuestions[index];
-    document.querySelector("#question-number").innerHTML = questionNumber;
-    document.querySelector("#player-score").innerHTML = playerScore;
-    document.querySelector("#display-question").innerHTML = currentQuestion.question;
-    document.querySelector("option-one-label").innerHTML = currentQuestion.option1;
-    document.querySelector("option-two-label").innerHTML = currentQuestion.option2;
-    document.querySelector("option-three-label").innerHTML = currentQuestion.option3;
-    document.querySelector("option-four-label").innerHTML = currentQuestion.option4;
+    document.getElementById("#question-number").innerHTML = questionNumber;
+    document.getElementById("#player-score").innerHTML = playerScore;
+    document.getElementById("#display-question").innerHTML = currentQuestion.title;
+    document.getElementById("#option-one-label").innerHTML = currentQuestion.option1;
+    document.getElementById("#option-two-label").innerHTML = currentQuestion.option2;
+    document.getElementById("#option-three-label").innerHTML = currentQuestion.option3;
+    document.getElementById("#option-four-label").innerHTML = currentQuestion.option4;
 }
 
 //function to check answers
@@ -160,35 +157,36 @@ function checkForAnswer() {
         if(option.value === currentQuestionAnswer) {
             correctOption = option.labels[0].id;
         }
-});
+    });
 //checking to make sure a radio input has been checked or an option being chosen
-if (options[0].checked === false && options[1].checked === false && options[2].checked === false && options[3].checked == false) {
-    document.getElementById("#option-modal").style.display = "flex";
+    if (options[0].checked === false && options[1].checked === false && options[2].checked === false && options[3].checked == false) {
+    document.getElementById("#option-modal");
 }
 
 //checking if checked radio button is same as answer
-options.forEach((option) => {
-    if (option.checked === true && option.value === currentQuestionAnswer) {
-        document.getElementById(correctOption).style.backgroundColor = "green";
-        playerScore++; //adding to player's score
-        indexNumber++; //adding 1 to index so has to display next question..
-        //set to delay question number till when next question loads
-        setTimeout(() => {
-            questionNumber++;
-        }, 1000);
-    }
+    options.forEach((option) => {
+        if (option.checked === true && option.value === currentQuestionAnswer) {
+            document.getElementById(correctOption).style.backgroundColor = "green";
+            playerScore++; //adding to player's score
+            indexNumber++; //adding 1 to index so has to display next question..
+            //set to delay question number till when next question loads
+            setTimeout(() => {
+                questionNumber++;
+            }, 1000);
+        }
 
-    else if (option.checked && option.value !== currentQuestionAnswer) {
-        const wrongLabelId = option.labels[0].id;
-        document.getElementById(wrongLabelId).style.backgroundColor = "red";
-        document.getElementById(correctOption).style.backgroundColor = "green";
-        indexNumber++;
-        //set to delay question number till when next question loads
-        setTimeout(() => {
-            questionNumber++;
-        }, 1000);
-    }
-});
+        else if (option.checked && option.value !== currentQuestionAnswer) {
+            const wrongLabelId = option.labels[0].id;
+            document.getElementById(wrongLabelId).style.backgroundColor = "red";
+            document.getElementById(correctOption).style.backgroundColor = "green";
+            indexNumber++;
+            //set to delay question number till when next question loads
+            setTimeout(() => {
+                questionNumber++;
+            }, 1000);
+        }
+    });
+}
 
 
 //called when the next button is called
@@ -198,10 +196,11 @@ function handleNextQuestion() {
     //delays next question displaying for a second just for some effects so questions don't rush in on player
     setTimeout(() => {
         if (indexNumber <= 7) {
-            NextQuestion(indexNumber);
+            nextQuestion(indexNumber);
         }
         else {
             handleEndGame();
+            goToResults();
         }
         resetOptionBackground();
     }, 1000);
@@ -212,7 +211,7 @@ function resetOptionBackground() {
     const options = document.getElementsByName("option");
     options.forEach((option) => {
         document.getElementById(option.labels[0].id).style.backgroundColor = "";
-    });
+    })
 }
 
 // unchecking all radio buttons for next question(can be done with map or foreach loop also)
@@ -223,11 +222,26 @@ function unCheckRadioButtons() {
     }
 }
 
-// function for when all questions being answered
+//go to results page
+function goToResult() {
+    var goToResultBox = document.querySelector("#result-box")
+     if (goToQuizBox.style.display !== "block") {
+         goToQuizBox.style.display = "block";
+    } else {
+        goToQuizBox.style.display = "none";
+        goToResultBox.style.display = "block";
+    }
+};
+
 function handleEndGame() {
     let remark = null;
     let remarkColor = null;
-
+    document.getElementById("#remarks").innerHTML = remark;
+    document.getElementById("#remarks").style.color = remarkColor;
+    document.getElementById("#grade").innerHTML = playerGrade;
+    document.getElementById("#result-time-left").innerHTML = timeLeftOver;
+    document.getElementById("#result-score").innerHTML = playerScore;
+    document.getElementById("#score-modal").style.display = "flex";
     // condition check for player remark and remark color
     if (playerScore <= 3) {
         remark = "Keep trying!";
@@ -235,23 +249,14 @@ function handleEndGame() {
     }
     else if (playerScore >= 4 && playerScore < 7) {
         remark = "You can do better.";
-        remarkColor = "orange";
+        remarkColor = "yellow";
     }
     else if (playerScore >= 7) {
         remark = "Excellent!";
-        remarkColor = "green";
+        remarkColor = "blue";
         prompt("New High Score! Enter your initials below to be added to the leaderboard!");
     }
-    const playerGrade = (playerScore / 10) * 100;
-
-    //data to display to score board
-    document.getElementById("#remarks").innerHTML = remark;
-    document.getElementById("#remarks").style.color = remarkColor;
-    document.getElementById("#grade").innerHTML = playerGrade;
-    document.getElementById("#result-time-left").innerHTML = timeLeftOver;
-    document.getElementById("#result-score").innerHTML = playerScore;
-    document.getElementById("#score-modal").style.display = "flex";
-
+    const playerGrade = (playerScore / 8) * 100;
 }
 
 //function for restart button(id="restart-btn")-> navigate from result box to rules box (id="rules-box")
@@ -264,7 +269,7 @@ restartBtn.onclick = () => {
     wrongAttempt = 0;
     indexNumber = 0;
     shuffledQuestions = [];
-    NextQuestion(indexNumber); {
+    NextQuestion(indexNumber);
     document.getElementById("#score-modal").style.display = "none";
     if (goToRulesBox.style.display !== "none") {
         goToRulesBox.style.display = "none";
@@ -274,7 +279,7 @@ restartBtn.onclick = () => {
     }
 };
 
-//function for quit button (id="quit-btn") -> navigate from result box to start page (id="start-page")
+// quit button (id="quit-btn") -> navigate from result box to start page (id="start-page")
 var quitBtn = document.querySelector("#quit-btn");
 var goToStartPage = document.querySelector("#start-page");
 quitBtn.onclick = () => {
@@ -284,7 +289,3 @@ quitBtn.onclick = () => {
         window.location.reload();
     }
 };
-
-//Code Sources: https://sebhastian.com/javascript-show-hide-div-onclick-toggle/
-//https://github.com/seogram/Simple-Score-keeper/tree/master/js
-//https://dev.to/sulaimonolaniran/building-a-simple-quiz-with-html-css-and-javascript-4elp//
